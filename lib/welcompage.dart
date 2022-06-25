23a54a0a743d4ef80905dc60191a9e40709932cb
Narasingha <Narasingha.das@capgemini.com>
2022-06-25 11:35:13 +0530

    Update welcompage.dart
    Modified the welcome Page

--- lib/welcompage.dart
+++ lib/welcompage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:oms/login/login.dart';
 
 void main() => runApp(Welcom());
 
@@ -23,7 +25,7 @@ class WelcomPage extends StatelessWidget {
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage(
-              'assets/register.jpg',
+              'assets/login.png',
             ),
             fit: BoxFit.cover,
           ),
@@ -32,13 +34,28 @@ class WelcomPage extends StatelessWidget {
           backgroundColor: Colors.transparent,
           body: Stack(
             children: [
-              Row(
+              Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
                     padding: EdgeInsets.only(
-                      top: 60.0,
+                      top: 80.0,
                     ),
+                    child: Text(
+                      'Welcome to the best ToDo baby !!',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                            letterSpacing: 3,
+                            fontSize: 20,
+                            wordSpacing: 2,
+                          ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: 80.0,
+                    ),
+
                     // child: Text(
                     //   'Welcom \n To ToDo Application',
                     //   textAlign: TextAlign.center,
@@ -47,15 +64,25 @@ class WelcomPage extends StatelessWidget {
                     //     fontSize: 40.0,
                     //   ),
                     // ),
+                    // child: Text(
+                    //   'Welcome to the best Task manager baby !!',
+                    //   textAlign: TextAlign.center,
+                    //   style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                    //         letterSpacing: 3,
+                    //         fontSize: 12,
+                    //         wordSpacing: 2,
+                    //       ),
+                    // ),
 
-                    child: Text(
-                      'Welcome to the best Task manager baby !',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                            letterSpacing: 3,
-                            fontSize: 12,
-                            wordSpacing: 2,
-                          ),
+                    child: RaisedButton(
+                      onPressed: () => {
+                        //do something
+                        Navigator.push(
+                            context,
+                            CupertinoPageRoute(
+                                builder: (context) => myLogin())),
+                      },
+                      child: new Text('Next'),
                     ),
                   )
                 ],
