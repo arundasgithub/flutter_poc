669a0b82ffde42ca3a6ab70761972a9ec43f3285
Amitha123456 <62466386+Amitha123456@users.noreply.github.com>
2022-06-29 10:11:59 +0530

    Update welcompage.dart
    added debugShowCheckedModeBanner

--- lib/welcompage.dart
+++ lib/welcompage.dart
@@ -9,6 +9,7 @@ class Welcom extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Home',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
